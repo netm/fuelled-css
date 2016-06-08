86300cc2586a356a669b12bdee1988603b8785e9
Gwilym <gwilymgj@gmail.com>
2016-06-08 22:06:07 +1200

    re-cache

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,23 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var nunjucksRender = require('gulp-nunjucks-render');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
+});
+
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('./app/pages/**/*.+(html|nunjucks)')
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['./app/templates']
+    }))
+  // output files in app folder
+  .pipe(gulp.dest('./app'))
 });
 
 gulp.task('sass:watch', function () {
